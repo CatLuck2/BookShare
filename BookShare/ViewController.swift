7d9c4c267175329684a9eb26e84c4a540cf9581a
CatLuck2 <freeaddress834@gmail.com>
2019-01-11 13:50:30 +0900

    create project file

---
+++ BookShare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookShare
+//
+//  Created by 藤澤洋佑 on 2019/01/11.
+//  Copyright © 2019年 NEKOKICHI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

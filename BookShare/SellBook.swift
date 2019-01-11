33c4994822bfcbcf918ceec6cf12aeeacbbb0e21
CatLuck2 <freeaddress834@gmail.com>
2019-01-11 22:32:27 +0900

    adjusted StoryBoard

---
+++ BookShare/SellBook.swift
@@ -0,0 +1,30 @@
+//
+//  SellBook.swift
+//  BookShare
+//
+//  Created by 藤澤洋佑 on 2019/01/11.
+//  Copyright © 2019年 NEKOKICHI. All rights reserved.
+//
+
+import UIKit
+
+class SellBook: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

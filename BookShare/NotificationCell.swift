1d3fa8eb3b830b78a182704fdb34174b524a6ec0
CatLuck2 <freeaddress834@gmail.com>
2019-01-14 22:47:54 +0900

    adjust viewControllerUI on storyBoard

---
+++ BookShare/NotificationCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationCell.swift
+//  BookShare
+//
+//  Created by 藤澤洋佑 on 2019/01/12.
+//  Copyright © 2019年 NEKOKICHI. All rights reserved.
+//
+
+import UIKit
+
+class NotificationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
